eb19561d9b5800fa0bcde0fa57e446bfaab92f27
matsworld <info@matsworld.io>
2019-06-06 10:32:33 -0700

    Initial commit

---
+++ js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Nezapomněli jste na své jméno?",
+                    minlength: "Alepsoň 2 znaky"
+                },
+                subject: {
+                    required: "A co předmět?",
+                    minlength: "Alespoň 4 znaky"
+                },
+                number: {
+                    required: "come on, you have a number, don't you?",
+                    minlength: "Alepsoň 5 znaků"
+                },
+                email: {
+                    required: "Potřebovali bychom znát Váš email..."
+                },
+                message: {
+                    required: "Asi by nebylo od věci něco napsat...",
+                    minlength: "To je vše?"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
